97fca4c7a532e5de2e1193fc339605adf81dc1d8
Keyze Torres <jsantos.torres2006@gmail.com>
2026-04-13 21:56:15 -0300

    Update Sistema_de_Eventos_Musicais.sql

--- Sistema_de_Eventos_Musicais.sql
+++ Sistema_de_Eventos_Musicais.sql
@@ -75,12 +75,6 @@ CREATE TABLE cliente (
     email VARCHAR(100),
     createAT TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
     updateAT TIMESTAMP DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP
-
-ALTER TABLE cliente 
-ADD CONSTRAINT unique_cpf UNIQUE (cpf);
-
-ALTER TABLE cliente 
-ADD CONSTRAINT unique_email UNIQUE (email);
 );
 
 CREATE TABLE compra_venda (
@@ -101,78 +95,99 @@ ALTER TABLE evento MODIFY COLUMN duracao_dias INT;
 
 -- insert eventos
 INSERT INTO evento (nome, data_inicio, duracao_dias, data_fim, endereco, quant_artistas) VALUES
-("Festival Verão Bahia", "2026-01-10", 3, "2026-01-12", "Salvador - BA, Brasil", 6),
-("Rock na Praia", "2026-02-05", 2, "2026-02-06", "Florianópolis - SC, Brasil", 4),
-("Sertanejo Prime", "2026-03-15", 1, "2026-03-15", "Goiânia - GO, Brasil", 3),
-("Noite do Samba", "2026-04-02", 1, "2026-04-02", "Rio de Janeiro - RJ, Brasil", 5),
-("Festival de Jazz & Blues", "2026-04-20", 2, "2026-04-21", "Campos do Jordão - SP, Brasil", 4),
-("Eletro Beats Festival", "2026-05-10", 3, "2026-05-12", "São Paulo - SP, Brasil", 7),
-("Arraiá Musical", "2026-06-20", 2, "2026-06-21", "Caruaru - PE, Brasil", 6),
-("Forró das Antigas", "2026-06-25", 1, "2026-06-25", "Campina Grande - PB, Brasil", 3),
-("Indie Music Fest", "2026-07-08", 2, "2026-07-09", "Curitiba - PR, Brasil", 5),
-("Hip Hop Nation", "2026-08-12", 1, "2026-08-12", "São Paulo - SP, Brasil", 4),
-("Pagode & Cia", "2026-09-03", 1, "2026-09-03", "Belo Horizonte - MG, Brasil", 4),
-("Festival Gospel Brasil", "2026-09-18", 2, "2026-09-19", "Brasília - DF, Brasil", 6),
-("Clássicos do Rock", "2026-10-05", 2, "2026-10-06", "Porto Alegre - RS, Brasil", 5),
-("Axé Bahia Fest", "2026-11-10", 3, "2026-11-12", "Salvador - BA, Brasil", 8),
-("Pop Explosion", "2026-11-25", 1, "2026-11-25", "Rio de Janeiro - RJ, Brasil", 4),
-("Festival Multicultural", "2026-12-05", 3, "2026-12-07", "Recife - PE, Brasil", 7),
-("Noite Eletrônica", "2026-12-12", 1, "2026-12-12", "Balneário Camboriú - SC, Brasil", 3),
-("Réveillon Musical", "2026-12-30", 2, "2026-12-31", "Fortaleza - CE, Brasil", 6);
+("Festival Verão Bahia", "2026-01-10", 3, "2026-01-12", "Salvador - BA, Brasil", 6), -- 1
+("Rock na Praia", "2026-02-05", 2, "2026-02-06", "Florianópolis - SC, Brasil", 4), -- 2
+("Sertanejo Prime", "2026-03-15", 1, "2026-03-15", "Goiânia - GO, Brasil", 3), -- 3
+("Noite do Samba", "2026-04-02", 1, "2026-04-02", "Rio de Janeiro - RJ, Brasil", 5), -- 4
+("Festival de Jazz & Blues", "2026-04-20", 2, "2026-04-21", "Campos do Jordão - SP, Brasil", 4), -- 5
+("Eletro Beats Festival", "2026-05-10", 3, "2026-05-12", "São Paulo - SP, Brasil", 7), -- 6
+("Arraiá Musical", "2026-06-20", 2, "2026-06-21", "Caruaru - PE, Brasil", 6), -- 7
+("Forró das Antigas", "2026-06-25", 1, "2026-06-25", "Campina Grande - PB, Brasil", 3), -- 8
+("Indie Music Fest", "2026-07-08", 2, "2026-07-09", "Curitiba - PR, Brasil", 5), -- 9
+("Hip Hop Nation", "2026-08-12", 1, "2026-08-12", "São Paulo - SP, Brasil", 4), -- 10
+("Pagode & Cia", "2026-09-03", 1, "2026-09-03", "Belo Horizonte - MG, Brasil", 4), -- 11
+("Festival Gospel Brasil", "2026-09-18", 2, "2026-09-19", "Brasília - DF, Brasil", 6), -- 12
+("Clássicos do Rock", "2026-10-05", 2, "2026-10-06", "Porto Alegre - RS, Brasil", 5), -- 13
+("Axé Bahia Fest", "2026-11-10", 3, "2026-11-12", "Salvador - BA, Brasil", 8), -- 14
+("Pop Explosion", "2026-11-25", 1, "2026-11-25", "Rio de Janeiro - RJ, Brasil", 4), -- 15
+("Festival Multicultural", "2026-12-05", 3, "2026-12-07", "Recife - PE, Brasil", 7), -- 16
+("Noite Eletrônica", "2026-12-12", 1, "2026-12-12", "Balneário Camboriú - SC, Brasil", 3), -- 17
+("Réveillon Musical", "2026-12-30", 2, "2026-12-31", "Fortaleza - CE, Brasil", 6); -- 18
 
 -- insert artistas
 INSERT INTO artista (nome, idade, repertorio_musical, genero, eventoID) VALUES
-('Brenda Luz', 27, 'MPB com influências de samba', 'MPB', 1),
-('Caio Ribeiro', 35, 'Bossa nova e clássicos brasileiros', 'MPB', 1),
-('Daniela Prado', 31, 'MPB romântica', 'MPB', 1),
-('Felipe Nogueira', 29, 'MPB moderna autoral', 'MPB', 1),
-('Banda Solar', 26, 'Pop rock animado', 'Pop Rock', 2),
-('Julio César', 33, 'Pop internacional acústico', 'Pop', 2),
-('Banda Fúria', 28, 'Rock pesado autoral', 'Rock', 3),
-('Roberta Dias', 37, 'Sertanejo raiz feminino', 'Sertanejo', 4),
-('Henrique Lopes', 41, 'Moda de viola tradicional', 'Sertanejo', 4),
-('Trio Nordestino Raiz', 45, 'Forró tradicional instrumental', 'Forró', 5),
-('DJ Flash', 34, 'EDM e remix de hits', 'Eletrônica', 6),
-('MC Bruninho', 22, 'Funk paulista', 'Funk', 6),
-('DJ Carla', 30, 'House e dance', 'Eletrônica', 6),
-('MC Léo', 25, 'Funk consciente', 'Funk', 6),
-('Grupo Raiz do Samba', 39, 'Samba tradicional', 'Samba', 7),
-('Quarteto Azul', 36, 'Jazz instrumental moderno', 'Jazz', 8),
-('DJ Wave', 31, 'Tech house', 'Eletrônica', 9),
-('DJ Luna', 27, 'Progressive house', 'Eletrônica', 9),
-('DJ Sky', 29, 'Electro house', 'Eletrônica', 9),
-('DJ Fire', 35, 'Techno industrial', 'Eletrônica', 9),
-('Banda Axé Power', 38, 'Axé animado carnaval', 'Axé', 10),
-('Tati Bahia', 33, 'Axé e pop baiano', 'Axé', 10),
-('Swing do Pelô', 40, 'Pagodão baiano', 'Axé', 10),
-('Axé Mix', 36, 'Clássicos do axé', 'Axé', 10),
-('Banda Energia', 37, 'Axé elétrico', 'Axé', 10),
-('MC Raiz', 28, 'Rap underground', 'Hip Hop/Rap', 11),
-('Banda Aurora', 26, 'Indie rock nacional', 'Indie', 12),
-('Luna Reis', 24, 'Folk alternativo', 'Indie/Folk', 12),
-('Old School Band', 45, 'Rock clássico anos 70', 'Rock Clássico', 13),
-('Ministério Vida', 42, 'Louvor congregacional', 'Gospel', 14),
-('Cantora Sara', 34, 'Gospel contemporâneo', 'Gospel', 14),
-('Pop Stars BR', 23, 'Pop dançante', 'Pop', 15),
-('Kelly Moraes', 29, 'Pop romântico', 'Pop', 15),
-('Dance Crew', 27, 'Pop eletrônico', 'Pop', 15),
-('Vitor Klein', 31, 'Pop internacional', 'Pop', 15),
-('Fusion Recife', 38, 'Mistura de ritmos brasileiros', 'MPB', 16),
-('Ritmo Livre', 35, 'World music', 'MPB', 16),
-('Coletivo Som', 33, 'Experimental brasileiro', 'MPB', 16),
-('Grupo Cultural PE', 41, 'Regional nordestino', 'MPB', 16),
-('Batida Urbana', 28, 'Mistura urbana', 'Hip Hop', 16),
-('Nação Musical', 37, 'Afro-brasileiro', 'MPB', 16),
-('Som Brasilis', 39, 'Instrumental brasileiro', 'MPB', 16),
-('DJ Night', 32, 'Eletrônica noturna', 'Eletrônica', 17),
-('DJ Vibe', 29, 'Dance music', 'Eletrônica', 17),
-('DJ ElectroMax', 34, 'EDM', 'Eletrônica', 17),
-('Banda Virada', 40, 'Hits variados ao vivo', 'Pop', 18),
-('Grupo Festa', 36, 'Música para eventos', 'Pop', 18),
-('DJ Celebration', 33, 'Remixes festivos', 'Eletrônica', 18),
-('Cantor Lucas', 28, 'Pop nacional', 'Pop', 18),
-('Energia Show', 38, 'Show ao vivo animado', 'Pop', 18),
-('Banda Réveillon', 42, 'Clássicos e atuais', 'Pop', 18);
+('Brenda Luz', 27, 'MPB com influências de samba', 'MPB', 1), -- 1
+('Caio Ribeiro', 35, 'Bossa nova e clássicos brasileiros', 'MPB', 1), -- 2
+('Daniela Prado', 31, 'MPB romântica', 'MPB', 1), -- 3
+('Felipe Nogueira', 29, 'MPB moderna autoral', 'MPB', 1), -- 4
+('Banda Solar', 26, 'Pop rock animado', 'Pop Rock', 2), -- 5
+('Julio César', 33, 'Pop internacional acústico', 'Pop', 2), -- 6
+('Banda Fúria', 28, 'Rock pesado autoral', 'Rock', 3), -- 7
+('Roberta Dias', 37, 'Sertanejo raiz feminino', 'Sertanejo', 4), -- 8
+('Henrique Lopes', 41, 'Moda de viola tradicional', 'Sertanejo', 4), -- 9
+('Trio Nordestino Raiz', 45, 'Forró tradicional instrumental', 'Forró', 5), -- 10
+('DJ Flash', 34, 'EDM e remix de hits', 'Eletrônica', 6), -- 11
+('MC Bruninho', 22, 'Funk paulista', 'Funk', 6), -- 12
+('DJ Carla', 30, 'House e dance', 'Eletrônica', 6), -- 13
+('MC Léo', 25, 'Funk consciente', 'Funk', 6), -- 14
+('Grupo Raiz do Samba', 39, 'Samba tradicional', 'Samba', 7), -- 15
+('Quarteto Azul', 36, 'Jazz instrumental moderno', 'Jazz', 8), -- 16
+('DJ Wave', 31, 'Tech house', 'Eletrônica', 9), -- 17
+('DJ Luna', 27, 'Progressive house', 'Eletrônica', 9), -- 18
+('DJ Sky', 29, 'Electro house', 'Eletrônica', 9), -- 19
+('DJ Fire', 35, 'Techno industrial', 'Eletrônica', 9), -- 20
+('Banda Axé Power', 38, 'Axé animado carnaval', 'Axé', 10), -- 21
+('Tati Bahia', 33, 'Axé e pop baiano', 'Axé', 10), -- 22
+('Swing do Pelô', 40, 'Pagodão baiano', 'Axé', 10), -- 23
+('Axé Mix', 36, 'Clássicos do axé', 'Axé', 10), -- 24
+('Banda Energia', 37, 'Axé elétrico', 'Axé', 10), -- 25
+('MC Raiz', 28, 'Rap underground', 'Hip Hop/Rap', 11), -- 26
+('Banda Aurora', 26, 'Indie rock nacional', 'Indie', 12), -- 27
+('Luna Reis', 24, 'Folk alternativo', 'Indie/Folk', 12), -- 28
+('Old School Band', 45, 'Rock clássico anos 70', 'Rock Clássico', 13), -- 29
+('Ministério Vida', 42, 'Louvor congregacional', 'Gospel', 14), -- 30
+('Cantora Sara', 34, 'Gospel contemporâneo', 'Gospel', 14), -- 31
+('Pop Stars BR', 23, 'Pop dançante', 'Pop', 15), -- 32
+('Kelly Moraes', 29, 'Pop romântico', 'Pop', 15), -- 33
+('Dance Crew', 27, 'Pop eletrônico', 'Pop', 15), -- 34
+('Vitor Klein', 31, 'Pop internacional', 'Pop', 15), -- 35
+('Fusion Recife', 38, 'Mistura de ritmos brasileiros', 'MPB', 16), -- 36
+('Ritmo Livre', 35, 'World music', 'MPB', 16), -- 37
+('Coletivo Som', 33, 'Experimental brasileiro', 'MPB', 16), -- 38
+('Grupo Cultural PE', 41, 'Regional nordestino', 'MPB', 16), -- 39
+('Batida Urbana', 28, 'Mistura urbana', 'Hip Hop', 16), -- 40
+('Nação Musical', 37, 'Afro-brasileiro', 'MPB', 16), -- 41
+('Som Brasilis', 39, 'Instrumental brasileiro', 'MPB', 16), -- 42
+('DJ Night', 32, 'Eletrônica noturna', 'Eletrônica', 17), -- 43
+('DJ Vibe', 29, 'Dance music', 'Eletrônica', 17), -- 44
+('DJ ElectroMax', 34, 'EDM', 'Eletrônica', 17), -- 45
+('Banda Virada', 40, 'Hits variados ao vivo', 'Pop', 18), -- 46
+('Grupo Festa', 36, 'Música para eventos', 'Pop', 18), -- 47
+('DJ Celebration', 33, 'Remixes festivos', 'Eletrônica', 18), -- 48
+('Cantor Lucas', 28, 'Pop nacional', 'Pop', 18), -- 49
+('Energia Show', 38, 'Show ao vivo animado', 'Pop', 18), -- 50
+('Banda Réveillon', 42, 'Clássicos e atuais', 'Pop', 18); -- 51
+
+-- insert participações
+INSERT INTO participacao (artistaID, eventoID) VALUES
+(1, 1), (2, 1), (3, 1), (4, 1),
+(5, 2), (6, 2),
+(7, 3),
+(8, 4), (9, 4),
+(10, 5),
+(11, 6), (12, 6), (13, 6), (14, 6),
+(15, 7),
+(16, 8),
+(17, 9), (18, 9), (19, 9), (20, 9),
+(21, 10), (22, 10), (23, 10), (24, 10), (25, 10),
+(26, 11),
+(27, 12), (28, 12),
+(29, 13),
+(30, 14), (31, 14),
+(32, 15), (33, 15), (34, 15), (35, 15),
+(36, 16), (37, 16), (38, 16), (39, 16), (40, 16), (41, 16), (42, 16),
+(43, 17), (44, 17), (45, 17),
+(46, 18), (47, 18), (48, 18), (49, 18), (50, 18), (51, 18);
 
 -- insert clientes
 INSERT INTO cliente (nome, idade, cpf, telefone, email) VALUES 
@@ -206,8 +221,8 @@ SET data_fim = '2026-03-24'
 WHERE id = 1;
 
 UPDATE evento 
-SET quant_artistas = 9
-WHERE nome = 'Festival Verão Bahia';
+SET quant_artistas = 16 
+WHERE nome = 'Lollapalooza';
 
 -- select sugestão
 -- Consultas Simples
