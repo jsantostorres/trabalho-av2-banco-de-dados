a865334066d8843f7d37a8652a41287d3c3d8941
Keyze Torres <jsantos.torres2006@gmail.com>
2026-04-14 12:51:03 -0300

    Update artist count for Festival Verão Bahia

--- Sistema_de_Eventos_Musicais.sql
+++ Sistema_de_Eventos_Musicais.sql
@@ -306,8 +306,8 @@ SET data_fim = '2026-03-24'
 WHERE id = 1;
 
 UPDATE evento 
-SET quant_artistas = 16 
-WHERE nome = 'Lollapalooza';
+SET quant_artistas = 10
+WHERE nome = 'Festival Verão Bahia';
 
 -- select sugestão
 -- Consultas Simples
